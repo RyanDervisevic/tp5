90a49c75c4d8dfacf791c70b7c44a5b8ee26f90d
RyanDervisevic <ryan.dervisevic@gmail.com>
2022-02-21 15:33:50 +0100

    first commit

---
+++ src/tp5/tp5.java
@@ -0,0 +1,11 @@
+package tp5;
+
+public class tp5 {
+   public static void main (String[] args){
+	   String lCalcul = "";
+	   System.out.println(checkCalcul(lCalcul));
+   }
+   public static boolean checkCalcul(String pCalcul) {
+	   return true;
+   }
+}
