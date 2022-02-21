7576d7ea6290d9f1f61dcb93bed91a4d22f37de3
Tatevik Galstyan <tatevik100@yahoo.com>
2022-02-21 15:45:21 +0100

    Commit modification code

--- src/tp5/tp5.java
+++ src/tp5/tp5.java
@@ -4,8 +4,12 @@ public class tp5 {
    public static void main (String[] args){
 	   String lCalcul = "";
 	   System.out.println(checkCalcul(lCalcul));
+      System.out.println(addition(5,6))
    }
    public static boolean checkCalcul(String pCalcul) {
 	   return true;
+   }
+   public static int addition(int i, int a){
+      return a+b
    }
 }
